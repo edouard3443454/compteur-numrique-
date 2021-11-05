acefb7767d8697d6d30ec252afe227d2714d4ff3
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-11-05 11:52:16 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Personne = Personne + 1
+    basic.showNumber(Personne)
+})
+input.onButtonPressed(Button.AB, function () {
+    Personne = 0
+    basic.showNumber(Personne)
+})
+input.onButtonPressed(Button.B, function () {
+    Personne = Personne - 1
+    basic.showNumber(Personne)
+})
+let Personne = 0
+Personne = 0
+basic.forever(function () {
+	
+})
